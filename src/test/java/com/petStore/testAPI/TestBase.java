3e1d81670d0819fea904c2e1276d6e45941e6b7d
nihat <mehmet_nihat_cerit@hotmail.com>
2023-05-22 22:44:49 +0300

    Test base import updated

--- src/test/java/com/petStore/testAPI/TestBase.java
+++ src/test/java/com/petStore/testAPI/TestBase.java
@@ -1,7 +1,5 @@
 package com.petStore.testAPI;
 
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.AfterClass;
